248ab133fb8414bf522116b0cb4c59d3ab5a8fe5
Subhrajoti Chakrabarty <148115965+Subhrajoti@users.noreply.github.com>
2024-03-07 10:36:31 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,8 @@
+let testImage = document.geteElementByd("test_image");
+let scrollValue = window.addEventListener("click", function(){
+    return window.scrollY
+});
+if (scrollValue===0){
+    testImage.style.margin="";
+};
+console.log(scrollValue);
