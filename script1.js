98bab2f48262a1d9668c5028226f9d87a3e13ba2
Subhrajoti Chakrabarty <148115965+Subhrajoti@users.noreply.github.com>
2024-07-23 20:07:14 +0530

    Delete script1.js

--- script1.js
+++
@@ -1,9 +0,0 @@
-let testImage = document.getElementById("testImage");
-
-let scrollValue = window.addEventListener("click", function(){
-    return window.scrollY
-});
-if (scrollValue===0){
-    testImage.style.marginTop="10px";
-};
-console.log(scrollValue);
